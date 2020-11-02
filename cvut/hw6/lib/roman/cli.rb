8e761aba525647a0b165d0b9b9313bd5111fc686
budikpet <sgt.petrov@gmail.com>
2020-11-03 00:28:43 +0100

    Bugfixes. Added method options.
    Now uses method options as is instructed.
    Shows help if no options provided.

--- cvut/hw6/lib/roman/cli.rb
+++ cvut/hw6/lib/roman/cli.rb
@@ -5,10 +5,22 @@ require 'thor'
 module Roman
   # Thor CLI interface for Roman CLI utility.
   class CLI < Thor
-    desc 'run', 'Runs the Roman CLI utility.'
-    def self.run(*args)
-      puts 'Lorem Ipsum blah blah blah'
-      puts args
+    desc '', 'Runs the Roman CLI utility.'
+    method_option :arabic, type: :string, aliases: '-a', required: false
+    method_option :roman, type: :string, aliases: '-r', required: false
+    def roman
+      arabic = options[:arabic]
+      roman = options[:roman]
+      if arabic.nil? && roman.nil?
+        CLI.command_help(Thor::Base.shell.new, 'roman')
+        return
+      end
+
+      puts 'Did give these options:'
+      puts arabic
+      puts roman
     end
+
+    default_task :roman
   end
 end
