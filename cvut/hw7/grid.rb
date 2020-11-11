d8296cc15aa5f5c6b03a7675ff5f7897df48f3b3
budikpet <sgt.petrov@gmail.com>
2020-11-11 18:34:16 +0100

    Game can now be loaded.

--- cvut/hw7/grid.rb
+++ cvut/hw7/grid.rb
@@ -4,47 +4,52 @@
 class Grid
   include Enumerable
 
+  EMPTY_VAL = 0
+
   # Create Sudoku game grid of given dimension
-  def initialize(dimension);
+  def initialize(dimension)
+    @dimension = dimension
+    @data = Array.new(dimension) { Array.new(dimension) { EMPTY_VAL } }
   end
 
   # Return string with game board in a console friendly format
-  def to_s(width = 3);
-  end
+  def to_s(width = 3); end
 
   # First element in the sudoku grid
-  def first;
+  def first
+    @data[0][0]
   end
 
   # Last element in the sudoku grid
-  def last;
+  def last
+    @data[-1][-1]
   end
 
   # Return value at given position
-  def value(x, y);
+  def value(x, y)
+    @data[x][y]
   end
 
   # Marks number +z+ which shouldn't be at position [x, y]
-  def exclude(x, y, z);
-  end
+  def exclude(x, y, z); end
 
   # True when there is already a number
-  def filled?(x, y);
+  def filled?(x, y)
+    @data[x][y] != EMPTY_VAL
   end
 
   # True when no game was loaded
-  def empty?;
-  end
+  def empty?; end
 
   # Yields elements in given row
-  def row_elems(x);
+  def row_elems(x)
     return enum_for(:row_elems, x) unless block_given?
 
     # TODO: more code here
   end
 
   # Yields elements in given column
-  def col_elems(y);
+  def col_elems(y)
     return enum_for(:col_elems, y) unless block_given?
 
     # TODO: more code here
@@ -52,7 +57,7 @@ class Grid
 
   # Yields elements from block which is
   # containing element at given position
-  def block_elems(x, y);
+  def block_elems(x, y)
     return enum_for(:block_elems, x, y) unless block_given?
 
     # TODO: more code here
@@ -60,41 +65,54 @@ class Grid
 
   # With one argument return row, with 2, element
   # at given position
-  def [](*args);
+  def [](*args)
+    x = args[0]
+    y = args[1]
+    args.size == 1 ? @data[x] : @data[x][y]
   end
 
   # With one argument sets row, with 2 element
-  def []=(*args);
+  def []=(*args)
+    x = args[0]
+
+    if args.size == 2
+      @data[x] = args[1]
+    else
+      y = args[1]
+      @data[x][y] = args[2]
+    end
   end
 
   # Return number of missing numbers in grid
-  def missing;
+  def missing
+    @data.flatten(1).select { |num| num == EMPTY_VAL }.size
   end
 
   # Number of filled cells
-  def filled;
+  def filled
+    rows * cols - missing
   end
 
   # Number of rows in this sudoku
-  def rows;
+  def rows
+    @dimension
   end
 
   # Number of columns in this sudoku
-  def cols;
+  def cols
+    @dimension
   end
 
   # Iterates over all elements, left to right, top to bottom
-  def each;
+  def each
     return enum_for(:each) unless block_given?
 
     # TODO: more code here
   end
 
   # Return true if no filled number break sudoku rules
-  def valid?;
-  end
+  def valid?; end
 
   # Serialize grid values to a one line string
-  def solution;
-  end
+  def solution; end
 end
--- cvut/hw7/string_parser.rb
+++ cvut/hw7/string_parser.rb
@@ -7,12 +7,43 @@ class StringParser
   class << self
     # Return true if passed object
     # is supported by this loader
+    # Supports strings of exactly 81 numbers either [0-9] or ([1-9] and ".") where 0 or "." are empty spaces.
     def supports?(arg)
+      return false if arg.nil?
+      return false unless arg.is_a? String
+      return false unless arg.size == 81
+
+      if arg.match(/\D/)
+        # Contains non-numeric character
+        return arg.scan(/\D/).reject { |c| c == '.' }.size.zero?
+      end
+
+      true
     end
 
     # Return Grid object when finished
     # parsing
+    # @param arg [String] a supported String value where first character is  [0,0], second is [0,1]...
     def load(arg)
+      return nil unless supports? arg
+
+      dimension = 9
+      grid = Grid.new(dimension)
+
+      x = 0
+      y = 0
+
+      arg.each_char do |char|
+        grid[x][y] = char.to_i
+
+        y += 1
+        if (y % dimension).zero?
+          y = 0
+          x += 1
+        end
+      end
+
+      grid
     end
   end
 end
