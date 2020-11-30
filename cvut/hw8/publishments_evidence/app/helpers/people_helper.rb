77d73293722d1cc1d23c1e92553dd316dc0d81e0
budikpet <sgt.petrov@gmail.com>
2020-11-30 01:01:11 +0100

    Person detail updated
    Person can now see his publications and go to them.

--- cvut/hw8/publishments_evidence/app/helpers/people_helper.rb
+++ cvut/hw8/publishments_evidence/app/helpers/people_helper.rb
@@ -2,4 +2,8 @@ module PeopleHelper
   def school
     School.find(@person.school_id)
   end
+
+  def linked_publications
+    Publication.all.select { |publication| Person.find(publication.person_id).username == @person.username }
+  end
 end
